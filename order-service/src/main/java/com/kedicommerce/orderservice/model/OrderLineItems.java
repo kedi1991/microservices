01c212c34277a70c26a06095476cf33c76500299
Okurut Henry Kedi <okurut.henry@yahoo.com>
2024-01-21 06:02:19 +0100

    add order and orderlineitems class to the order-service model

---
+++ order-service/src/main/java/com/kedicommerce/orderservice/model/OrderLineItems.java
@@ -0,0 +1,26 @@
+package com.kedicommerce.orderservice.model;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "order_line_items")
+@Getter
+@Setter
+public class OrderLineItems {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String skuCode;
+    private BigDecimal price;
+    private Integer quantity;
+}
