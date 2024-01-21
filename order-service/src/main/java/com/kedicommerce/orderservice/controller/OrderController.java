297c34ba18870167f8a1dac50f44eb7ff8931c21
Okurut Henry Kedi <okurut.henry@yahoo.com>
2024-01-21 18:00:39 +0100

    set db properties file, and call the service method to write to the db

--- order-service/src/main/java/com/kedicommerce/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/kedicommerce/orderservice/controller/OrderController.java
@@ -1,19 +1,25 @@
 package com.kedicommerce.orderservice.controller;
 
 import com.kedicommerce.orderservice.dto.OrderRequest;
+import com.kedicommerce.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/order")
+@RequiredArgsConstructor
 public class OrderController {
     /**
      * place an order using a dto object
      * @return
      */
-    public String placeOrder(@RequestBody OrderRequest orderRequest){
 
+    OrderService orderService;
+    public String placeOrder(@RequestBody OrderRequest orderRequest){
+        //call the service to make the order
+        orderService.placeOrder(orderRequest);
         return "order placed";
     }
 }
--- order-service/src/main/java/com/kedicommerce/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/kedicommerce/orderservice/service/OrderService.java
@@ -4,16 +4,20 @@ import com.kedicommerce.orderservice.dto.OrderLineItemsDto;
 import com.kedicommerce.orderservice.dto.OrderRequest;
 import com.kedicommerce.orderservice.model.Order;
 import com.kedicommerce.orderservice.model.OrderLineItems;
+import com.kedicommerce.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class OrderService {
     /**
      * used to place the order with a DTO
      */
+    OrderRepository orderRepository;
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -24,6 +28,7 @@ public class OrderService {
                 .toList();
 
         order.setOrderLineItemsList(orderLineItemsList);
+        orderRepository.save(order);
     }
 
     private OrderLineItems mapToOrderLineItems(OrderLineItemsDto orderLineItemsDto) {
